920b26bf7297754e66c444f5bd45e0f82b2155e8
mrfarhankhan007 <fk95034@gmail.com>
2024-05-27 14:15:28 +0530

    added use warmupbrowser and clerk authentication code

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
 
 const Login = () => {
+  useWarmUpBrowser()
+  
   return (
     <View>
       <Text>Login Page</Text>
