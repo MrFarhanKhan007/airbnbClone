2b86a9126072dd90c3346a97f1dbee92b523c428
mrfarhankhan007 <fk95034@gmail.com>
2024-05-27 14:21:29 +0530

    added style for login page

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,15 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
 import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
 
 const Login = () => {
   useWarmUpBrowser()
-  
+
   return (
-    <View>
-      <Text>Login Page</Text>
+    <View style={styles.container}>
+      <TextInput autoCapitalize='none' placeholder='Email'>
+        
+      </TextInput>
     </View>
   )
 }
 
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      padding: 26
+    }
+  }
+)
 export default Login
