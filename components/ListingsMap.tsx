afe155ef931f83699999b38323685b557fef901d
mrfarhankhan007 <fk95034@gmail.com>
2024-06-08 00:16:36 +0530

    implemented map clusters

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,8 +1,10 @@
 import { View, StyleSheet, Text } from 'react-native'
 import React from "react"
-import MapView, { Marker } from "react-native-maps"
+import { Marker } from "react-native-maps"
 import { defaultStyles } from '@/constants/Styles'
 import { AirbnbListing } from '@/assets/data/airbnblistingsinterface'
+import { useRouter } from 'expo-router'
+import MapView from "react-native-map-clustering"
 
 interface Props {
     listings: any
@@ -17,6 +19,37 @@ const INITIAL_REGION = {
 
 const ListingsMap = ({ listings }: Props) => {
 
+    const router = useRouter()
+    const onMarkerSelected = (item: AirbnbListing) => {
+        router.push(`/listing/${item.id}`)
+    }
+
+  // Overwrite the renderCluster function to customize the cluster markers
+  const renderCluster = (cluster: any) => {
+    const { id, geometry, onPress, properties } = cluster;
+
+    const points = properties.point_count;
+    return (
+      <Marker
+        key={`cluster-${id}`}
+        coordinate={{
+          longitude: geometry.coordinates[0],
+          latitude: geometry.coordinates[1],
+        }}
+        onPress={onPress}>
+        <View style={styles.marker}>
+          <Text
+            style={{
+              color: '#000',
+              textAlign: 'center',
+              fontFamily: 'mon-sb',
+            }}>
+            {points}
+          </Text>
+        </View>
+      </Marker>
+    );
+  };
 
     return (
         <View style={defaultStyles.container}>
@@ -28,18 +61,29 @@ const ListingsMap = ({ listings }: Props) => {
                 showsCompass={true}
                 showsScale={true}
                 region={INITIAL_REGION}
+                clusterColor='#FFF'
+                clusterTextColor='#000'
+                clusterFontFamily='mon-sb'
+                renderCluster={renderCluster}
             >
                 {
                     listings.map((item: AirbnbListing) => (
                         <Marker
                             key={item.id}
+                            onPress={() => { onMarkerSelected(item) }}
                             coordinate={{
                                 latitude: item.latitude,
                                 longitude: item.longitude
                             }}
                             title={item.name}
                             description={item.description}
-                        />
+                        >
+                            <View style={styles.marker}>
+                                <Text style={styles.markerText}>
+                                    {item.price}
+                                </Text>
+                            </View>
+                        </Marker>
                     ))
                 }
             </MapView>
@@ -49,10 +93,45 @@ const ListingsMap = ({ listings }: Props) => {
 }
 
 
-const styles = StyleSheet.create(
-    {
-
-    }
-)
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    marker: {
+        padding: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        elevation: 5,
+        borderRadius: 12,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        shadowOffset: {
+            width: 1,
+            height: 10,
+        },
+    },
+    markerText: {
+        fontSize: 14,
+        fontFamily: 'mon-sb',
+    },
+    locateBtn: {
+        position: 'absolute',
+        top: 70,
+        right: 20,
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 10,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        shadowOffset: {
+            width: 1,
+            height: 10,
+        },
+    },
+})
 
 export default ListingsMap
